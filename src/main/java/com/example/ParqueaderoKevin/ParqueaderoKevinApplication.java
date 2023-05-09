1c74224a9f2bf69d4e866bbfe40831b7082fd414
Esteban <kecastellanos-2021b@corhuila.edu.co>
2023-05-08 22:04:14 -0500

    Spring Boot
    Añadi las extenciones necesesarias del proyecto

---
+++ src/main/java/com/example/ParqueaderoKevin/ParqueaderoKevinApplication.java
@@ -0,0 +1,13 @@
+package com.example.ParqueaderoKevin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParqueaderoKevinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParqueaderoKevinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ParqueaderoKevin/ParqueaderoKevinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ParqueaderoKevin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParqueaderoKevinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
